239a8974b54d3c0bd2bcb3cdc53a2ba4229f77ab
Harshita Gupta <sign.of.happiness@gmail.com>
2023-11-10 18:07:40 +0530

    implemented delete note functionality in DisplayNote.jsx

--- src/components/DisplayNote.jsx
+++ src/components/DisplayNote.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import "./DisplayNote.css";
+import { useDispatch } from "react-redux";
+import { deleteNote } from "../features/noteSlice";
 
 const DisplayNote = ({ note }) => {
+  const dispatch = useDispatch();
+
   return (
     <div>
       {
         <li className="note">
           <h2>{note.title}</h2>
           <div className="note-description">{note.description}</div>
+          <div
+            className="delete-note"
+            onClick={() => dispatch(deleteNote(note.id))}
+          >
+            🗑
+          </div>
         </li>
       }
     </div>
